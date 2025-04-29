d0cae228101145b1f09e496ed54295a90adaf480
Awais <awaismehmood88@gmail.com>
2025-04-29 20:11:21 +0400

    chore:

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -7,17 +7,9 @@ module.exports = {
       autorestart: true,
       watch: false,
       max_memory_restart: '1G',
-      env: {
-        NODE_ENV: 'development',
-        PORT: 5000
-      },
-      env_production: {
-        NODE_ENV: 'production',
-        PORT: 5000
-      },
       error_file: 'logs/error.log',
       out_file: 'logs/output.log',
-      time: true // Add timestamps to logs
-    }
-  ]
+      time: true, // Add timestamps to logs
+    },
+  ],
 };
